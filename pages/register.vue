bb0949ed6ded341de5483e9618862a608bf3b0bb
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 02:32:41 +0300

    New register page

---
+++ pages/register.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "authentication",
+});
+</script>
+
+<template>
+  <v-card >
+    <v-card-text>
+      <FormsAuthenticationRegisterForm />
+    </v-card-text>
+  </v-card>
+</template>
