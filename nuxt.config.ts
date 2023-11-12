d5bed6886782d37c08188f69e5e0055ccb285bf6
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 02:22:59 +0300

    turn off ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   runtimeConfig: {
     nodeEnv: process.env.NUXT_NODE_ENV || "development",
     public: {
