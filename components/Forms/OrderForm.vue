52341db4c34d5991cb512abc72461d762de88ec3
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 02:59:13 +0300

    New order form | Included form in page

---
+++ components/Forms/OrderForm.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import { reactive } from "vue";
+
+interface FormState {
+  product_id: number;
+  buyer_id: number;
+  cost: number;
+  quantity: number;
+  payment_status: boolean;
+  dispatch_status: boolean;
+  delivery_status: boolean;
+}
+
+const formState = reactive<FormState>({
+  product_id: 0,
+  buyer_id: 0,
+  cost: 0,
+  quantity: 0,
+  payment_status: false,
+  dispatch_status: false,
+  delivery_status: false,
+});
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+</script>
+
+<template>
+  <a-form
+    :model="formState"
+    name="basic"
+    :label-col="{ span: 8 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+      label="Product"
+      name="product_id"
+    >
+      <a-input v-model:value="formState.product_id" />
+    </a-form-item>
+
+    <a-form-item
+      label="Buyer"
+      name="buyer_id"
+    >
+      <a-input v-model:value="formState.buyer_id" />
+    </a-form-item>
+
+    <a-form-item
+      label="Cost"
+      name="cost"
+    >
+      <a-input v-model:value="formState.cost" />
+    </a-form-item>
+
+    <a-form-item
+      label="Quantity"
+      name="quantity"
+    >
+     <a-input v-model:value="formState.quantity" />
+    </a-form-item>
+
+    <a-form-item
+      label="Payment status"
+      name="payment_status"
+    >
+     <a-input v-model:value="formState.payment_status" />
+    </a-form-item>
+
+    <a-form-item
+      label="Dispatch status"
+      name="dispatch_status"
+    >
+     <a-input v-model:value="formState.dispatch_status" />
+    </a-form-item>
+
+    <a-form-item
+      label="Delivery status"
+      name="delivery_status"
+    >
+     <a-input v-model:value="formState.delivery_status" />
+    </a-form-item>
+
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
--- pages/orders.vue
+++ pages/orders.vue
@@ -19,42 +19,49 @@ const columns = ref<TableColumnsType>([
     dataIndex: "id",
     key: "id",
     resizable: true,
+    width: 50
   },
   {
     title: "Buyer",
     dataIndex: "buyer_id",
     key: "buyer_id",
     resizable: true,
+    width: 200
   },
   {
     title: "Seller",
     dataIndex: "seller_id",
     key: "seller_id",
     resizable: true,
+    width: 200
   },
   {
     title: "Product",
     dataIndex: "product_id",
     key: "product_id",
     resizable: true,
+    width: 200
   },
   {
     title: "Payment status",
     dataIndex: "payment_status",
     key: "payment_status",
     resizable: true,
+    width: 100
   },
   {
     title: "Dispatch status",
     dataIndex: "dispatch_status",
     key: "dispatch_status",
     resizable: true,
+    width: 100
   },
   {
     title: "Delivery status",
     dataIndex: "delivery_status",
     key: "delivery_status",
     resizable: true,
+    width: 100
   },
   {
     title: "Actions",
@@ -66,6 +73,26 @@ const columns = ref<TableColumnsType>([
 function handleResizeColumn(w: any, col: any) {
   col.width = w;
 }
+
+const showEditOrder = ref<boolean>(false);
+
+const showDeleteOrder = ref<boolean>(false);
+
+const openEditOrder = (order_id: string | number) => {
+  showEditOrder.value = true;
+  console.log("order", order_id);
+};
+
+const openDeleteOrder = (
+  order_id: string | number
+) => {
+  showDeleteOrder.value = true;
+  console.log("order", order_id);
+};
+
+const saveEditedOrder = () => {
+  showEditOrder.value = false;
+};
 </script>
 
 <template>
@@ -150,11 +177,48 @@ function handleResizeColumn(w: any, col: any) {
 
         <!-- Actions -->
         <template v-if="column.key === 'action'">
-          <DeleteOutlined style="cursor: pointer" />
+          <DeleteOutlined style="cursor: pointer"
+
+          @click="openDeleteOrder(record.id)"
+
+          />
           <a-divider type="vertical" />
-          <EditOutlined style="cursor: pointer" />
+          <EditOutlined style="cursor: pointer"
+
+          @click="openEditOrder(record.id)"
+/>
         </template>
       </template>
     </a-table>
+
+
+
+    <!-- ---------------------------------------------- -->
+    <!--Edit order -->
+    <!-- ---------------------------------------------- -->
+    <a-drawer
+      title="Edit order"
+      :open="showEditOrder"
+      @close="saveEditedOrder"
+    >
+      <FormsOrderForm />
+    </a-drawer>
+    <!-- ---------------------------------------------- -->
+    <!--Delete order modal -->
+    <!-- ---------------------------------------------- -->
+    <a-drawer
+      title="Delete order"
+      :open="showDeleteOrder"
+      :footer-style="{ textAlign: 'right' }"
+      @close="showDeleteOrder = false"
+    >
+      <p>Do you want to delete this order?</p>
+      <a-space>
+        <a-button @click="showDeleteOrder = false">Cancel</a-button>
+        <a-button type="primary" @click="showDeleteOrder = false"
+          >Submit</a-button
+        >
+      </a-space>
+    </a-drawer>
   </div>
 </template>
