c0a35823785376a2b16ec8d515453fca4e40debb
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 02:32:57 +0300

    New auth layout

---
+++ layouts/authentication.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main class="fill-height">
+      <v-container class="h-100" style="padding: 0px !important; height: 100% !important;">
+        <section style="height: 100% !important;">
+          <v-row style="height: 100% !important; display: flex; justify-content: center !important; align-items: center !important;">
+            <v-col cols="auto">
+              <slot />
+            </v-col>
+          </v-row>
+        </section>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
