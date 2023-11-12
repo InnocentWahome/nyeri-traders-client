9c67d930039eeba297ed87ae0a834195934c9e12
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 02:23:11 +0300

    New login page and login form

---
+++ components/Forms/Authentication/LoginForm.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const { login, loginFormState } = useAuthentication();
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+</script>
+
+<template>
+  <h1>Login Form</h1>
+  <a-form
+    :model="loginFormState"
+    name="basic"
+    :label-col="{ span: 8 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+      label="Email"
+      name="email"
+      :rules="[{ required: true, message: 'Please input your email!' }]"
+    >
+      <a-input v-model:value="loginFormState.email" />
+    </a-form-item>
+
+    <a-form-item
+      label="Password"
+      name="password"
+      :rules="[{ required: true, message: 'Please input your password!' }]"
+    >
+      <a-input-password v-model:value="loginFormState.password" />
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" @click="login()">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
