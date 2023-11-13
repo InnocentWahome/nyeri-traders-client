0d73f290bf47e7ec540e8ca4c4db7f745abdbaf7
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 03:04:04 +0300

    New categories form

---
+++ components/Forms/CategoriesForm.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+import { reactive } from "vue";
+
+interface FormState {
+  name: string;
+  description: string;
+}
+
+const formState = reactive<FormState>({
+  name: "",
+  description: "",
+});
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+</script>
+
+<template>
+  <a-form
+    :model="formState"
+    name="basic"
+    :label-col="{ span: 8 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    @finish="onFinish"
+    @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+      label="Name"
+      name="name"
+      :rules="[{ required: true, message: 'Please input your name!' }]"
+    >
+      <a-input v-model:value="formState.name" />
+    </a-form-item>
+
+    <a-form-item
+      label="Description"
+      name="description"
+      :rules="[{ required: true, message: 'Please input your description!' }]"
+    >
+      <a-input v-model:value="formState.description" />
+    </a-form-item>
+
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
--- pages/categories.vue
+++ pages/categories.vue
@@ -19,18 +19,22 @@ const columns = ref<TableColumnsType>([
     dataIndex: "id",
     key: "id",
     resizable: true,
+    width: 50
   },
   {
     title: "Name",
     dataIndex: "name",
     key: "name",
     resizable: true,
+    width: 300
   },
   {
     title: "Description",
     dataIndex: "description",
     key: "description",
     resizable: true,
+    width: 600,
+    ellipsis: true
   },
   {
     title: "Actions",
@@ -42,6 +46,24 @@ const columns = ref<TableColumnsType>([
 function handleResizeColumn(w: any, col: any) {
   col.width = w;
 }
+
+const showEditCategory = ref<boolean>(false);
+
+const showDeleteCategory = ref<boolean>(false);
+
+const openEditCategory = (category_id: string | number) => {
+  showEditCategory.value = true;
+  console.log("category", category_id);
+};
+
+const openDeleteCategory = (category_id: string | number) => {
+  showDeleteCategory.value = true;
+  console.log("category", category_id);
+};
+
+const saveEditedCategory = () => {
+  showEditCategory.value = false;
+};
 </script>
 
 <template>
@@ -54,15 +76,48 @@ function handleResizeColumn(w: any, col: any) {
       :scroll="{ x: 2000 }"
       :expand-column-width="100"
     >
-      <template #bodyCell="{ column }">
-
+      <template #bodyCell="{ column, record }">
         <!-- Actions -->
         <template v-if="column.key === 'action'">
-          <DeleteOutlined style="cursor: pointer" />
+          <DeleteOutlined
+            style="cursor: pointer"
+            @click="openDeleteCategory(record.id)"
+          />
           <a-divider type="vertical" />
-          <EditOutlined style="cursor: pointer" />
+          <EditOutlined
+            style="cursor: pointer"
+            @click="openEditCategory(record.id)"
+          />
         </template>
       </template>
     </a-table>
+
+    <!-- ---------------------------------------------- -->
+    <!--Edit category -->
+    <!-- ---------------------------------------------- -->
+    <a-drawer
+      title="Edit category"
+      :open="showEditCategory"
+      @close="saveEditedCategory"
+    >
+      <FormsCategoriesForm />
+    </a-drawer>
+    <!-- ---------------------------------------------- -->
+    <!--Delete category modal -->
+    <!-- ---------------------------------------------- -->
+    <a-drawer
+      title="Delete category"
+      :open="showDeleteCategory"
+      :footer-style="{ textAlign: 'right' }"
+      @close="showDeleteCategory = false"
+    >
+      <p>Do you want to delete this category?</p>
+      <a-space>
+        <a-button @click="showDeleteCategory = false">Cancel</a-button>
+        <a-button type="primary" @click="showDeleteCategory = false"
+          >Submit</a-button
+        >
+      </a-space>
+    </a-drawer>
   </div>
 </template>
