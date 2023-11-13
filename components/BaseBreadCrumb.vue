795b2e4531cb16761a5498b9c1bc1507dce7cc86
InnocentWahome <innocentwahome@gmail.com>
2023-11-13 03:55:22 +0300

    Update breadcrumb path

--- components/BaseBreadCrumb.vue
+++ components/BaseBreadCrumb.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const capitalizeFirstLetter = (str: any) => str.charAt(0).toUpperCase() + str.slice(1);
+</script>
+
 <template>
   <a-breadcrumb style="margin: 16px 0">
     <a-breadcrumb-item>Home</a-breadcrumb-item>
-    <a-breadcrumb-item>List</a-breadcrumb-item>
-    <a-breadcrumb-item>App</a-breadcrumb-item>
+    <a-breadcrumb-item>{{ capitalizeFirstLetter(router.currentRoute.value.name) }}</a-breadcrumb-item>
   </a-breadcrumb>
 </template>
